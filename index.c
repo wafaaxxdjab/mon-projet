4ad2db614337d6c43e081cf9b56e0853e36d0750
foufa djab <wafadjaballah123@gmail.com>
2024-01-03 17:36:36 +0100

    first commit

--- index.c
+++ index.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 
 int main()
-{  
+{   
+    int T[100];
+    int i,n;
+    
     printf("donner la taille de tableau");
+    scanf("%d,&n");
+    printf("donner les elements de tabbleau");
+    for(i=0; i<=n; i++){
+       scanf("%d",T[i]);
+    }
     return 0;
 
 }
