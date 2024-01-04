e1d4803b1e0ca71995dfefa5077b7362413e2012
foufa djab <wafadjaballah123@gmail.com>
2024-01-04 21:20:48 +0100

    first commit

--- index.c
+++ index.c
@@ -4,13 +4,14 @@
 void tri_bulle(int arr[],int n){
     int i,j,X;
     for(i=0; i <n-1; i++){
-         for(j=0; j <n-i-1; j++){
-          if(arr[j]>arr[j+1]){
-            X=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=X;
-          }
-      }
+       for(j=0; j <n-i-1; j++){
+          if(arr[j]> arr[j+1]){
+                 X = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = X;
+            }
+           
+        }
     }
 }
 int main()
