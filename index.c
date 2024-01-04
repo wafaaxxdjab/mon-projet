babbed837fcebe0ef55c2d4837e78734160c9d22
foufa djab <wafadjaballah123@gmail.com>
2024-01-04 15:44:24 +0100

    first commit

--- index.c
+++ index.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void tri_bulle(int arr[],int taille){
+    int i,j,X,N;
+    for(i=0; i <N-1; i++){
+         for(j=0; j <N-i-1; j++){
+          if(arr[j]>arr[j+1]){
+            X=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=X;
+          }
+      }
+    }
+}
 int main()
 {   
     int T[100];
